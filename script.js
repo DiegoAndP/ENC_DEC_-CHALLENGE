8f23f0e0386f310149f367890c77768f20ac2e61
DiegoAndP <98297437+DiegoAndP@users.noreply.github.com>
2022-01-24 15:23:48 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,56 @@
+let contem = /[A-Z0-9áéíóúÁÉÍÓÚãÃõÕ]/g;
+let botoes= document.querySelectorAll(".btn");
+//let texto = "";
+botoes[0].onclick = codifica;
+botoes[1].onclick = decodifica;
+//botoes[2].onclick = copia;
+
 function codifica() {
 
-	
-	if (contem.test(insiraTexto.value) == true) {
+	let insiraTexto = document.querySelectorAll(".text-input");
+
+	let x = insiraTexto[0];
+	let y = insiraTexto[1];
+
+	if (contem.test(x.value) == true) {
 
 		alert('Este programa não aceita letras maiúsculas, com acentos ou números.')
 
 	} else {
 
-		let texto = insiraTexto.value.replace("e", "enter")
+		texto = x.value.replace("e", "enter")
 			.replace("i", "imes")
 			.replace("a", "ai")
 			.replace("o", "ober")
 			.replace("u", "ufat");
 
-		msgbox.value = texto;	
-		return mensagem = texto;
+		return y.value = texto;	
+		
 	}
 }
 
 
 function decodifica() {
 
-	if (contem.test(insiraTexto.value) == true) {
+	let insiraTexto = document.querySelectorAll(".text-input");
 
-		alert('Este programa não aceita letras maiúsculas acentos ou números.')
+	let x = insiraTexto[0];
+	let y = insiraTexto[1];
 
+	if (contem.test(x.value) == true) {
+
+		alert('Este programa não aceita letras maiúsculas, com acentos ou números.')
 	} else {
 
-		let texto = insiraTexto.value.replace("ufat", "u")
+		texto = insiraTexto.replace("ufat", "u")
 			.replace("ober", "o")
 			.replace("ai", "a")
 			.replace("imes", "i")
 			.replace("enter", "e");
 
-		msgbox.value = texto;
-		return resultado = texto;
-
+		return y.value = texto;
+		
 	}
 }
-let resultado = "";
-let msgbox = document.getElementById("msg");
-let contem = /[A-Z0-9áéíóúÁÉÍÓÚãÃõÕ]/;
-let insiraTexto = document.getElementById("input-texto");
-let botaoEncripta = document.getElementById("btn-cripto");
-let botaoDecripta = document.getElementById("btn-descripto");
-botaoDecripta.onclick = decodifica;
-botaoEncripta.onclick = codifica;
 
-console.log(resultado);
 
